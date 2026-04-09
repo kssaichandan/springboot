c6c7668fca47d3442581fe2852a47df33a60f0af
Batch1 <Batch1@VITUNIVERSITY.LOCAL>
2026-04-09 08:17:31 +0530

    Initial commit of Spring Boot K8s project

---
+++ src/main/java/com/example/HelloController.java
@@ -0,0 +1,9 @@
+package com.example;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String home() { return "App Running Successfully!"; }
+}
